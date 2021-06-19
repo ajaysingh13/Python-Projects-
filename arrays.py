53fd186904989e66da6c2fafabc947c3dbe3b93e
ajaysingh13 <86167628+ajaysingh13@users.noreply.github.com>
2021-06-19 12:29:33 -0700

    arrays and first assignment
    learned how to use arrays

---
+++ arrays.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  8 18:44:57 2021
+
+@author: vijay
+"""
+
+import numpy as np 
+
+'''
+array1 = np.array([1, 2, 3])
+
+array2d = np.array([[1, 2, 3], [4, 5, 6]])
+
+list1 = [1,2,3]
+
+print(list1)
+print(array2d)
+print (array1) 
+
+array2 = np.array([3, 6 , 2, 1])
+
+array6 = np.array([5, 6, 7, 8])
+
+print(array6 * 2)
+print(array2 + 1)
+'''
+'''
+list1 = [1, 2, 3, 4, 5]
+list2 = [6, 7, 8, 9, 10]
+
+arr1 = np.array(list1)
+arr2 = np.array(list2)
+
+print(arr1)
+print(arr2)
+
+arr3 = arr1 * arr2 
+
+print(arr3)
+'''
+
+a = np.arange(0, 21 , 2)
+print(a) 
+
+b = np.arange(5, 15.1, .1)
+print(b)
+
+a = np.arange(1, 21)
+print(a)
+
+print(a[12])
+
