7a316f072b8d3e87f8c73322d826ca06ae6ec334
ajaysingh13 <86167628+ajaysingh13@users.noreply.github.com>
2021-06-28 11:02:16 -0700

    Importing file test and information about functions

---
+++ MoarFunctionInfo.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  8 18:09:11 2021
+
+@author: vijay
+"""
+
+
+
+def multiply (x,y):
+    p = (x * y)
+    return p
+
+def addingNumba(g,y):
+    total = g + y
+    return total
+
+'''
+j = addingnumba(3,5)
+h = multiply(8)
+'''           
---
+++ import test.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  8 18:20:34 2021
+
+@author: vijay
+"""
+
+import MoarFunctionInfo as f1
+
+x = f1.addingNumba(5, 5)
+print(x)
